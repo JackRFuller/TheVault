13915c5c950d38b64dc6106968250cc8b92f0767
JackRFuller <jackrfuller@outlook.com>
2016-12-23 13:26:11 +0000

    Added MonoSingleton Structure

---
+++ Project/TheVault/Assets/Scripts/ScriptableObjects/LevelData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Level Data", menuName = "ScriptableObject/Level Data",order = 1)]
+public class LevelData : ScriptableObject
+{
+    [SerializeField]
+    private GameObject levelPrefab;
+    public GameObject LevelPrefab
+    {
+        get
+        {
+            return levelPrefab;
+        }
+    }
+
+    [SerializeField]
+    private float levelStartingTime;
+    public float LevelStartTime
+    {
+        get
+        {
+            return levelStartingTime;
+        }
+    }
+
+    [SerializeField]
+    private int[] starRatingTargets = new int[3];
+    public int[] StarRatingTargets
+    {
+        get
+        {
+            return starRatingTargets;
+        }
+    }
+}
