e1f84fe9c3a1ddbab764c4cd9188fb9e457fccc7
JackRFuller <jackrfuller@outlook.com>
2017-01-11 21:16:00 +0000

    Art Experiments

---
+++ Project/TheVault/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoSingleton<GameManager>
+{
+
+    public override void UpdateNormal()
+    {
+        GameInput();
+    }
+
+    private void GameInput()
+    {
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if(Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
